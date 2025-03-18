8fe2f7f77a02cc51347c7a6830928ca7114a23bb
Jasper <jaspervermeulen@icloud.com>
2025-03-18 11:39:42 +0100

    update

---
+++ src/app/api/weer/route.js
@@ -0,0 +1,30 @@
+// app/api/avengers/route.js
+export async function GET(request) {
+  const users = [
+    {
+      id: 1,
+      city: 'Blankenberge',
+      temperature: '7º',
+      feelsLike: '5º',
+      min: '10º',
+      max: '5º',
+    },
+    {
+      id: 2,
+      name: 'Brugge',
+      temperature: '-2º',
+      feelsLike: '-3º',
+      min: '2º',
+      max: '-4º',
+    },
+    {
+      id: 3,
+      name: 'Knokke',
+      temperature: '24º',
+      feelsLike: '32º',
+      min: '20º',
+      max: '25º',
+    },
+  ];
+  return Response.json(users);
+}
